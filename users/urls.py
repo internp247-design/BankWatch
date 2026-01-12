6918eabfc30530c96ccc8c6ed4abe933bde9a177
internp247-design <internp247@gmail.com>
2026-01-12 11:09:49 +0530

    profile 2

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('profile/', views.profile, name='profile'),  # Add this
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
 ]
